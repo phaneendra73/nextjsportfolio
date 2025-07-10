f6b8832e03ff2078447546978b1c2bf01e33d2c5
Phaneendra Marri <118047850+phaneendra73@users.noreply.github.com>
2025-07-10 12:17:43 +0530

    Update Profile Photo

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -236,12 +236,14 @@ export const NavbarLogo = () => {
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
       <img
-        src="https://assets.aceternity.com/logo-dark.png"
-        alt="logo"
-        width={30}
-        height={30}
-      />
-      <span className="font-medium text-black dark:text-white">Startup</span>
+  src="https://avatars.githubusercontent.com/u/118047850?s=400&u=41e136972826a1c9e80f37f6aecbf2cc0ce975b1&v=4"
+  alt="logo"
+  width={30}
+  height={30}
+  className="rounded-full"
+/>
+
+      <span className=" text-2xl font-medium text-black dark:text-white">Phaneendra</span>
     </a>
   );
 };
